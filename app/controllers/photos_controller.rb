f2e7c21bfc9324f54e9985f55f7d7d30c5ab9853
Artem <skrartrom@icloud.com>
2020-05-01 23:53:06 +0300

    Add resorce(photo) with AJAX actions

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,57 @@
+class PhotosController < ApplicationController
+  before_action :set_photo, except: %i[index new create]
+
+  def index
+    @photos = Photo.all.order(id: :desc)
+  end
+
+  def show
+  end
+
+  def new
+    @photo = Photo.new
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def edit
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+    respond_to do |format|
+      if @photo.save
+        format.js
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @photo.update(photo_params)
+        format.js
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @photo.destroy
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  private
+    def set_photo
+      @photo = Photo.find(params[:id])
+    end
+
+    def photo_params
+      params.require(:photo).permit(:description)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_29_145217) do
+ActiveRecord::Schema.define(version: 2020_05_01_165238) do
+
+  create_table "photos", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
