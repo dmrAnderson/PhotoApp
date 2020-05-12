855f6da36b71c5acf90c2269c3ec589d50f9b078
Artem <skrartrom@icloud.com>
2020-05-12 23:58:36 +0300

    Added profile & reworked form for new photos

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,12 @@
+class ProfilesController < ApplicationController
+  before_action :find_user
+
+  def show
+  end
+
+  private
+
+    def find_user
+      @user = User.find(params[:id])
+    end
+end
