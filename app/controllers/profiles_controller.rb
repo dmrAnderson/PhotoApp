0184797c081e43f94d6c78f015a9250cc61a3758
Artem <skrartrom@icloud.com>
2020-05-14 01:12:31 +0300

    refactoring a subscription system

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,24 +1,37 @@
 class ProfilesController < ApplicationController
   before_action :find_user
+  before_action :double_sub,   only: [:subscribe]
+  before_action :double_unsub, only: [:unsubscribe]
+  before_action :sub_action_with_my_profile, except: [:show] 
+
 
   def show
   end
 
-  def update
-    @subscribe = current_user.subscriptions.build
-    @subscribe.update(friend_id: @user.id)
-    if @subscribe.save
-      redirect_to profile_path(@user), notice: 'true'
-    else
-      redirect_to profile_path(@user), notice: 'false'
-    end
+  def subscribe
+    current_user.subscriptions.create(friend_id: @user.id)
+    redirect_to profile_path(@user), notice: 'subscribe'
   end
 
-  def destroy
-    @unsubscribe
+  def unsubscribe
+    @unsubscribe = current_user.subscriptions.find_by(friend_id: @user.id)
+    @unsubscribe.destroy
+    redirect_to profile_path(@user), notice: 'unsubscribe'
   end
 
   private
+
+    def double_sub
+      redirect_to profile_path(@user) if current_user.subscriptions.exists?(friend_id: @user.id)
+    end
+
+    def double_unsub
+      redirect_to profile_path(@user) unless current_user.subscriptions.exists?(friend_id: @user.id)
+    end
+
+    def sub_action_with_my_profile
+      redirect_to profile_path(@user) if current_user == @user
+    end
 
     def find_user
       @user = User.find(params[:id])
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_194200) do
+ActiveRecord::Schema.define(version: 2020_05_13_212157) do
 
   create_table "photos", force: :cascade do |t|
     t.string "description"
